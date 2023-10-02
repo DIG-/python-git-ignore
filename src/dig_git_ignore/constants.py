8de343a8f6ce747329ce57da30c4cd881a21db36
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-10-02 17:29:05 -0300

    Created constants

---
+++ src/dig_git_ignore/constants.py
@@ -0,0 +1,10 @@
+""" Provides constants to be used in project """
+
+PROVIDER_URL = "https://gitignore.io/api/"
+
+GITIGNORE_FILENAME = ".gitignore"
+GITIGNORE_BACKUP = GITIGNORE_FILENAME + ".bak"
+GITIGNORE_DOWNLOAD = GITIGNORE_FILENAME + ".down"
+
+CONTENT_BLOCK_START = "# Created by http"
+CONTENT_BLOCK_END = "# End of http"
