1b2bacd40079c7e9b6be851722a8dec2e17136e5
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-10-03 01:24:51 -0300

    Created list_all

---
+++ src/dig_git_ignore/action/list_all.py
@@ -0,0 +1,13 @@
+from dig_git_ignore.constants import PROVIDER_URL
+from dig_git_ignore.provider import get_templates
+
+
+def list_all(url: str = PROVIDER_URL) -> None:
+    first_char: str = "?"
+    print("Listing all availables templates.", end="")
+    for name in sorted(get_templates(url)):
+        if name[0] != first_char:
+            first_char = name[0]
+            print(f"\n{first_char.upper()}:\n  {name}", end="")
+        else:
+            print(f", {name}", end="")
